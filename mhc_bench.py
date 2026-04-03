f41f724dfa5857fef8acfcb6da6d16f973ac50d4
Kaining Zhong <kainingz@nvidia.com>
2026-04-03 17:57:30 +0000

    whoops
    Signed-off-by: Kaining Zhong <kainingz@nvidia.com>

--- mhc_bench.py
+++ mhc_bench.py
@@ -184,7 +184,7 @@ def run_pre(B, T, n, C, dtype, device, do_backward):
 
 def run_post_res_triton(B, T, n, C, dtype, device, do_backward):
     nvtx.range_push("mhc_post_res_triton_fwd")
-    x = torch.randn(B, T, C, n dtype=dtype, requires_grad=True, device=device)
+    x = torch.randn(B, T, C, n, dtype=dtype, requires_grad=True, device=device)
     H_post = torch.randn(B, T, n, dtype=dtype, requires_grad=True, device=device)
     H_res = torch.randn(B, T, n, n, dtype=dtype, requires_grad=True, device=device)
     f = torch.randn(B, T, C, dtype=dtype, requires_grad=True, device=device)
