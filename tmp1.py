9a20af34f3deed198957088359412dd22bed6b0d
Kaining Zhong <kainingz@nvidia.com>
2026-05-18 23:53:40 +0000

    ???
    Signed-off-by: Kaining Zhong <kainingz@nvidia.com>

---
+++ tmp1.py
@@ -0,0 +1,23 @@
+import time
+
+from transformer_engine.pytorch.tensor.mxfp8_tensor import MXFP8Quantizer
+
+import transformer_engine_torch as tex
+import torch
+
+rowwise = True
+colwise = False
+M = 4096
+N = 4096
+
+quantizer = MXFP8Quantizer(
+    fp8_dtype=tex.DType.kFloat8E4M3,
+    rowwise=rowwise,
+    columnwise=colwise,
+)
+
+t0 = time.perf_counter_ns()
+for i in range(1000):
+    quantized_output = quantizer.make_empty((M, N), device="cuda")
+t1 = time.perf_counter_ns()
+print(f"Time: {(t1 - t0) / 1e9:.6f} seconds")
---
+++ tmp2.py
@@ -0,0 +1,24 @@
+import time
+
+from transformer_engine.pytorch.tensor.mxfp8_tensor import MXFP8Quantizer
+
+import transformer_engine_torch as tex
+import torch
+
+rowwise = True
+colwise = False
+M = 4096
+N = 4096
+
+quantizer = MXFP8Quantizer(
+    fp8_dtype=tex.DType.kFloat8E4M3,
+    rowwise=rowwise,
+    columnwise=colwise,
+)
+
+t0 = time.perf_counter_ns()
+for i in range(1000):
+    rowwise_data = torch.empty((M, N), dtype=torch.uint8, device="cuda")
+    rowwise_scale_inv = torch.empty((M, N // 32), dtype=torch.uint8, device="cuda")
+t1 = time.perf_counter_ns()
+print(f"Time: {(t1 - t0) / 1e9:.6f} seconds")
